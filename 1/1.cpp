1a17e3f8008873904a5ec7afd8039d4553aa8689
ReyhanAfshar <156189983+afsharz@users.noreply.github.com>
2024-04-07 13:55:40 +0330

    first try

--- 1/1.cpp
+++ 1/1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 class Person {
@@ -11,7 +12,32 @@ public:
 		this->name = name;
 
 	}
+	Person() { name = ""; age = 0; }
+	Person(const string& name,int age)
+	{
+		this->name = name; 
+		this->age = age;
+	}
+	bool operator > (Person& p)
+	{
+		if (name.compare(p.getName()) > 0)
+			return true;
+		return false;
+	}
+	bool operator < (Person& p)
+	{
+		if (name.compare(p.getName()) < 0)
+			return true;
+		return false;
+	}
+	bool operator == (const Person& p)
+	{
+		if (name.compare(p.getName()) == 0)
+			return true;
+		return false;
+	}
 	string& getName()  { return name; }
+	string getName() const { return name; }
 
 private:
 	string name;
@@ -25,14 +51,26 @@ ostream& operator<<(ostream& out, const Person& p) {
 }
 
 
-
 class Group {
 public:
-
+	Group(const Group& g) {
+		members = new Person[g.cap];
+		cap = g.cap;
+		size = 0;
+		for (int i = 0; i < g.size; i++)
+				this->add(g.members[i]);
+	}
 	Group(int max_lenght)  {
 		this->cap = max_lenght;
 		this->members = new Person[max_lenght];
 		this->size = 0;
+	}
+	void operator=(const Group& g) {
+		members = new Person[10];
+		cap = g.cap;
+		size = 0;
+		for (int i = 0; i < g.size; i++)
+			this->add(g.members[i]);
 	}
 	void add(Person p) {
 		if (this->size == this->cap) {
@@ -44,7 +82,7 @@ public:
 	void deletePerson(string name) {
 		int i;
 		for (i = 0; i < size; i++) {
-			if(members[i].name == name)
+			if(members[i].getName() == name)
 				break;
 			
 		}
@@ -54,6 +92,24 @@ public:
 		size--;
 	}
 
+	int getSize(){	return size;}
+	int getCap() { return cap; }
+	Person* getMembers(){ return members; }
+	Group operator+(Group& g)
+	{
+		Group output(cap + g.getCap());
+		for (int i = 0; i < cap; i++)
+			output.add(members[i]);
+		for (int i = 0; i < g.getCap(); i++)
+			output.add(g.getMembers()[i]);
+		return output;
+	}
+	Group& operator += (Group& g)
+	{
+		for (int i = 0; i < g.getCap(); i++)
+			this->add(g.getMembers()[i]);
+		return *this;
+	}
 	~Group() {
 		delete[] members;
 	}
@@ -64,62 +120,62 @@ private:
 	Person* members;
 
 };
-
-bool isGroupFull(Group g) {
-	return (g.size == g.cap);
+ostream& operator<<(ostream& out, Group& g)
+{
+	for (int i = 0; i < g.getSize(); i++)
+	{
+		cout << g.getMembers()[i].getName() << "\t";
+	}
+	return out;
 }
 
+
+
+
+bool isGroupFull(Group g) {
+	return (g.getSize() == g.getCap());
+}
+   
+/*****************************************/
 int main()
 {
-	/*
+	
 	Person p1("somename");
-	Person p1("somename2");
+	Person p2("somename2");
 	cout << p1<<p2;
-	*/
-	/*
+	
 	Group g1(5);
-	g1.add({ "Erfan",20 });
-	g1.add({ "Saba",20 });
-	g1.add({ "Mahrokh",20 });
-	g1.add({ "Yasin",21 });
+	g1.add({ "erfan",20 });
+	g1.add({ "saba",20 });
+	g1.add({ "mahrokh",20 });
+	g1.add({ "yasin",21 });
 	cout<<isGroupFull(g1)<<endl;
-	*/
-	/*
+	
+
 	Group g2(g1);
 	Group g3(10);
-	g2.deletePerson("Erfan");
-	cout << g1<<endl;//should cout every person in it with a \t as the separator
+	g2.deletePerson("erfan");
+	cout << g1<<endl;//should cout every Person in it with a \t as the separator
 	cout << g2 << endl;
-	*/
-	/*
+	
+	
 	g3 = g1;
 	cout << g3;
 	g3 =  g2 + g3;//adds members of g2 and g3 to each other
 	g3 += g3 += g3 += g3;
-	*/
-	/*
-	Person persons[10];
-	Person Erfan("Erfan",20);
-	persons[1]=Erfan;
-	for(int i=0;i<10;i++){
-		persons[i].getName()=to_string(rand()%100);
-	}
-	sort(persons,persons+10);//what operator should be overloaded for sort? :)
-
-	for(int i=0;i<10;i++){
-		cout<<persons[i].getName()<<' ';
-	}*/
-	/*
-	cout<<*find(persons,persons+10,Erfan);
-	*/
 	
+	Person Persons[10];
+	Person erfan("erfan",20);
+	Persons[1]=erfan;
+	for(int i=0;i<10;i++){
+		Persons[i].getName()=to_string(rand()%100);
+	}
+	sort(Persons,Persons+10);//what operator should be overloaded for sort? :) 
 
-
-
-
-
-
-
-
+	for(int i=0;i<10;i++){
+		cout<<Persons[i].getName()<<' ';
+	}
+	
+	cout<<find(Persons,Persons+10,erfan);
 }
 
